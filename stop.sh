550566e60fe7c1b1710b137ca74c0145672989cc
Sedin-Pooja <pooja@sedintechnologies.com>
2025-07-25 17:30:50 +0530

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -4,7 +4,7 @@ set -e
 CONTAINER_NAME="mycontainer-cicd"
 
 echo "Stopping container: $CONTAINER_NAME"
-docker stop "$CONTAINER_NAME"
+docker stop "$CONTAINER_NAME" || true
 
 echo "Removing container: $CONTAINER_NAME"
-docker rm "$CONTAINER_NAME" 
+docker rm "$CONTAINER_NAME" || true
