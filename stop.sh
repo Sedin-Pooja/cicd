58f7f9666e38ca0c388b8036c9ede7967150172e
Sedin-Pooja <pooja@sedintechnologies.com>
2025-07-25 17:16:43 +0530

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -3,11 +3,8 @@ set -e
 
 CONTAINER_NAME="mycontainer-cicd"
 
-echo "Stopping and removing container: $CONTAINER_NAME"
+echo "Stopping container: $CONTAINER_NAME"
+docker stop "$CONTAINER_NAME"
 
-if docker ps -q -f name="$CONTAINER_NAME" | grep -q .; then
-  docker stop "$CONTAINER_NAME"
-  docker rm "$CONTAINER_NAME"
-else
-  echo "No running container named $CONTAINER_NAME found."
-fi
+echo "Removing container: $CONTAINER_NAME"
+docker rm "$CONTAINER_NAME" 
