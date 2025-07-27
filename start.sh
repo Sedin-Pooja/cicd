4cc258b38ebdcc7b38b99728033d6eb1e6c2a287
Sedin-Pooja <pooja@sedintechnologies.com>
2025-07-27 18:47:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
-cd cicd/
 AWS_REGION="ap-south-1"
 ACCOUNT_ID="156916773321"
 REPO_NAME="pooja/rails_app"
 IMAGE_TAG="latest"
 CONTAINER_NAME="mycontainer-cicd"
 IMAGE_URI="${ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPO_NAME}:${IMAGE_TAG}"
+$ENV_FILE=".env"
 
 echo "Logging into AWS ECR..."
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin "${ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
@@ -15,7 +15,7 @@ echo "Pulling image: $IMAGE_URI"
 docker pull $IMAGE_URI
 
 echo "Running container: $CONTAINER_NAME"
-docker run -d --env-file /home/ec2-user/cicd/.env -p 3000:3000 --name $CONTAINER_NAME $IMAGE_URI
+docker run -d --env-file $ENV_FILE -p 3000:3000 --name $CONTAINER_NAME $IMAGE_URI
 
 
 
