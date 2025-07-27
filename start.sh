2f1fe3b250288380b26640a7caa7a0be6237883b
Sedin-Pooja <pooja@sedintechnologies.com>
2025-07-27 19:22:11 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,8 +6,6 @@ REPO_NAME="pooja/rails_app"
 IMAGE_TAG="latest"
 CONTAINER_NAME="mycontainer-cicd"
 IMAGE_URI="${ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPO_NAME}:${IMAGE_TAG}"
-APP_DIR="/home/ec2-user/cicd"
-MASTER_KEY_PATH="$APP_DIR/config/master.key"
 
 
 echo "Logging into AWS ECR..."
@@ -17,25 +15,18 @@ echo "Pulling image: $IMAGE_URI"
 docker pull $IMAGE_URI
 
 echo "Fetching RAILS_MASTER_KEY from SSM..."
-#to get master.key from ssm
-RAILS_MASTER_KEY=$(aws ssm get-parameter \
+export RAILS_MASTER_KEY=$(aws ssm get-parameter \
   --name "/pooja-cicd/master.key" \
   --with-decryption \
   --query "Parameter.Value" \
   --output text)
 
-mkdir -p "$APP_DIR/config"
-chmod 755 "$APP_DIR/config"
-chown ec2-user:ec2-user "$APP_DIR/config"
+echo "Stopping any existing container..."
+docker rm -f "$CONTAINER_NAME" 2>/dev/null || true
 
-
-echo "$RAILS_MASTER_KEY" > "$MASTER_KEY_PATH"
-chmod 600 "$MASTER_KEY_PATH"
-
-echo "Running Docker container..."
-
+echo "Running container..."
 docker run -d \
-  -v "$MASTER_KEY_PATH":/app/config/master.key \
+  -e RAILS_MASTER_KEY="$RAILS_MASTER_KEY" \
   -e RAILS_ENV=production \
   -p 3000:3000 \
   --name "$CONTAINER_NAME" \
