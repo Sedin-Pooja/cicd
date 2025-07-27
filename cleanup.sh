26bb9b15df7bf0373051887df81de87a070e58bc
Sedin-Pooja <pooja@sedintechnologies.com>
2025-07-27 20:02:54 +0530

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
-echo "Cleaning up old deployment files..."
-rm -rf /home/ec2-user/cicd/*
+TARGET_DIR="/home/ec2-user/cicd"
 
+echo "Fixing ownership of $TARGET_DIR..."
+sudo chown -R ec2-user:ec2-user "$TARGET_DIR"
+
+echo "Cleaning up old files..."
+rm -rf "$TARGET_DIR"/*
