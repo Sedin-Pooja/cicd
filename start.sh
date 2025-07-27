9872327dd0bd1fd619513cada34b2a9b8ff2a7e4
Sedin-Pooja <pooja@sedintechnologies.com>
2025-07-27 18:54:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -15,7 +15,7 @@ echo "Pulling image: $IMAGE_URI"
 docker pull $IMAGE_URI
 
 echo "Running container: $CONTAINER_NAME"
-docker run -d --env-file $ENV_FILE -p 3000:3000 --name $CONTAINER_NAME $IMAGE_URI
+docker run -d --env-file "$ENV_FILE" -p 3000:3000 --name "$CONTAINER_NAME" "$IMAGE_URI"
 
 
 
