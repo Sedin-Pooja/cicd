63168d36344b54edeb5e068936a3fb3bcbf17b82
Sedin-Pooja <pooja@RFL942.localdomain>
2025-07-30 12:00:39 +0000

    updating files

--- start.sh
+++ start.sh
@@ -7,24 +7,21 @@ IMAGE_TAG="latest"
 CONTAINER_NAME="mycontainer-cicd"
 IMAGE_URI="${ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPO_NAME}:${IMAGE_TAG}"
 
-
-echo "Logging into AWS ECR..."
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin "${ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
 
-echo "Pulling image: $IMAGE_URI"
 docker pull $IMAGE_URI
 
-echo "Fetching RAILS_MASTER_KEY from SSM..."
+echo "Fetching RAILS_MASTER_KEY from SSM"
 export RAILS_MASTER_KEY=$(aws ssm get-parameter \
   --name "/pooja-cicd/master.key" \
   --with-decryption \
   --query "Parameter.Value" \
   --output text)
 
-echo "Stopping any existing container..."
-docker rm -f "$CONTAINER_NAME" 2>/dev/null || true
+echo "Stopping any existing container"
+docker rm -f "$CONTAINER_NAME" || true
 
-echo "Running container..."
+echo "Running container"
 docker run -d \
   -e RAILS_MASTER_KEY="$RAILS_MASTER_KEY" \
   -e RAILS_ENV=production \
@@ -32,7 +29,7 @@ docker run -d \
   --name "$CONTAINER_NAME" \
   "$IMAGE_URI"
 
-echo "Container started."
+echo "Container started"
 
 
 
