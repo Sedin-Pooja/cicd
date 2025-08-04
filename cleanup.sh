af0d94aa483f2c931ca044371824dfdb9ba3d0c5
Sedin-Pooja <pooja@sedintechnologies.com>
2025-08-04 16:00:38 +0530

    Delete cleanup.sh

--- cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-TARGET_DIR="/home/ec2-user/cicd"
-
-echo "Fixing ownership perm"
-sudo chown -R ec2-user:ec2-user "$TARGET_DIR"
-
-echo "Cleaning up old files & hidden files"
-rm -rf "$TARGET_DIR"/{*,.*}
