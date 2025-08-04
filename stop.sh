51d0d14133462478303a43608d587a8fd72fc343
Sedin-Pooja <pooja@sedintechnologies.com>
2025-08-04 16:00:03 +0530

    Delete stop.sh

--- stop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-CONTAINER_NAME="mycontainer-cicd"
-
-echo "Stopping container"
-docker stop "$CONTAINER_NAME" || true
-
-echo "Removing container"
-docker rm "$CONTAINER_NAME" || true
